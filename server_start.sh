c6a77b0fea99c40e65bfc90f5745f48683618ade
Juan Duque <juanpablo.duque@polimi.it>
2024-06-28 14:31:16 +0200

    docker fixes

--- server_start.sh
+++ server_start.sh
@@ -9,5 +9,9 @@ python3 manage.py migrate --noinput
 #    --email $DJANGO_SUPERUSER_EMAIL
 python3 manage.py setup
 
-(gunicorn roseapi.wsgi -w 3 -b 0.0.0.0:8001 -t 90) &
+# Start the celery app
+celery -A roseapi worker --loglevel=info --detach
+
+# Start the django app at port - use environment variables for workers and timeout.
+(gunicorn roseapi.wsgi -w ${WEB_WORKERS:-1} -b 0.0.0.0:8001 -t ${WEB_TIMEOUT:-60}) &
 nginx -g "daemon off;"
